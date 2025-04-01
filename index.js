33e3f265fa55f57d4b75e7f709a2395389242f69
Casper Gamborg <Gamborgc@gmail.com>
2025-04-01 14:57:30 +0200

    Design almost finished - needs review.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+
+const firstNumberColumn = document.querySelector(".first-number-column");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(element => {
+    console.log(element.innerHTML);
+});
+
+console.log(buttons);
+
