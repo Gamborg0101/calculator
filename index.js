95360a9468f9fdfd4de6598e032ac97ac11732f8
Casper Gamborg <Gamborgc@gmail.com>
2025-04-01 15:01:13 +0200

    all elements now have an eventlistener. Need to add functionality and print on screen

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
 const firstNumberColumn = document.querySelector(".first-number-column");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach(element => {
+buttons.forEach((element) => {
+  element.addEventListener("click", () => {
     console.log(element.innerHTML);
+  });
 });
 
-console.log(buttons);
+/* 
+JEG FUCKING GJORDE DET UDEN AT SLÅ NOGET OP!! GENI!!*/
 
+console.log(buttons);
