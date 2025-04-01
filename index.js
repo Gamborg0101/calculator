d546c20861a1e24a38dc4bb90492ef8291c78523
Casper Gamborg <Gamborgc@gmail.com>
2025-04-01 17:58:31 +0200

    Logic updated with objects etc.

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
 const firstNumberColumn = document.querySelector(".first-number-column");
 const buttons = document.querySelectorAll("button");
 const screenLocation = document.querySelector(".calc-screen");
+const operators = document.querySelectorAll(".operation");
+
+operators.forEach((element) => {
+  element.addEventListener("click", () => {
+    let operation = element.getAttribute("x-operation");
+    let operationToString = operation.toString();
+    console.log(mathTable[operationToString]);
+  });
+});
+
+let mathTable = {
+  plus: (a, b) => a + b,
+  minus: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  division: (a, b) => a / b,
+};
 
 let screenNumbers = document.createElement("p");
 screenNumbers.classList.add("screen-number");
 screenNumbers.innerText = "";
 screenLocation.appendChild(screenNumbers);
 
+let firstValue = "";
+
 buttons.forEach((element) => {
   element.addEventListener("click", () => {
-    console.log(element.innerText);
     screenNumbers.innerText += element.innerText;
-
-    console.log(screenNumbers.innerText);
-    /*Dette kan jeg smide ind i min funktion, 
-    men hvad så når jeg bruger +?
-    screenNumbers.innerText = "" - men dette
-    fjerner vel også værdien fra funktionen? 
-
-    Jeg skal vel forsøge at gemme det i en variabel, men det kan jeg vil ikke,
-    når det er screenNumbers.innerText der skal ændres?
-    Og hvordan ændre jeg min variabel, når jeg klikker på "+"?
-    Måske skal jeg gemme screenNumbers.innerText i en variabel, og 
-    gøre det en del af et dobbelt if loop? 
-    Så if(value1 == "number")
-    if(value2 == "number")
-        sum(); 
-    else(
-    innerText = ""; 
-    )
-    
-
-    Vent... Vil det være bedre at bruge event.target.value? 
-    så - screenNUmbers.innerText += event.target.value;
-
-
-    
-    
-    */
-
-    if (element.innerText == "+") {
-      //sum function - but what about inputs?
-      sum();
-    }
+    firstValue = screenNumbers.innerText;
+    console.log(firstValue);
   });
 });
-
-const sum = (a, b) => {
-  return a + b;
-};
-
-/* 
-JEG FUCKING GJORDE DET UDEN AT SLÅ NOGET OP!! GENI!!*/
