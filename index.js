683d9d6aed5104d2778529dfd3e156e16d3bc6e9
Casper Gamborg <Gamborgc@gmail.com>
2025-04-03 10:56:40 +0200

    Funcionality is now working - need to be aware of edgecases

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const operators = document.querySelectorAll(".operation");
 const writeNumber = document.querySelector(".calcHeaderValue");
 const reset = document.querySelector(".reset");
 const numberButton = document.querySelectorAll(".number");
+const equalButton = document.querySelector(".operation.equal");
 
 let currentState = "value1";
 let value1 = "";
 let value2 = "";
+let operationsToString = "";
+let operation = "";
 
 /* clear the calc */
 reset.addEventListener("click", (e) => {
   writeNumber.innerText = "";
   value1 = "";
+  operationsToString = "";
   value2 = "";
   currentState = "value1";
 });
 
+equalButton.addEventListener("click", () => {
+  console.log(operationsToString);
+  console.log(value1);
+  console.log(value2);
+  console.log(operate(operationsToString, value1, value2));
+});
+
 /* calculator object*/
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   const operations = {
     plus: (a, b) => a + b,
     minus: (a, b) => a - b,
@@ -26,32 +39,37 @@ function operate(operator, a, b) {
   };
   return operations[operator](a, b);
 }
+console.log(operate("plus", 1, 4));
 
 /* operators */
 operators.forEach((element) => {
   element.addEventListener("click", () => {
-    let operation = element.getAttribute("x-operation");
-    let operationToString = operation.toString();
-    currentState = "value2";
     writeNumber.innerHTML = "";
-
-    //console.log(operate(operationToString, value1, value2));
+    operation = element.getAttribute("x-operation");
+    operationsToString = operation.toString();
+    currentState = "value2";
   });
 });
 
 /* Get values */
 numberButton.forEach((element) => {
-  element.addEventListener("click", (e, operationToString) => {
+  element.addEventListener("click", (e) => {
     let numberTarget = e.target.innerText;
     if (currentState == "value1") {
       value1 += numberTarget;
-      console.log("int one: " + value1);
+      console.log("value1: " + value1);
     } else if (currentState == "value2") {
       value2 += numberTarget;
-      console.log("int two: " + value2);
+      console.log("value2: " + value2);
     }
-    console.log("final values: ", value1, value2, operationToString);
   });
 });
+
+/* 
+Så skal jeg have sendt value 1 til den. 
+Så skal jeg have sendt operater til den
+Så skal jeg have sendt valyue 2 til den. //DEM MODTAGER JEG ALLEREDE,FORDI DE ER GLOBALE. 
+Så skal jeg køre operate funktionen. 
+*/
 
 //console.log("Info from bottom: " + operate("multiply", 1, 2));
