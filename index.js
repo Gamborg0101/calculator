54940bf260f21af26c8f20cf45c53d9a22d6701d
Casper Gamborg <Gamborgc@gmail.com>
2025-04-03 13:55:13 +0200

    Began with del function, and added notes

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const numberPrint = document.querySelector(".calcHeaderValue");
 const reset = document.querySelector(".reset");
 const numberButton = document.querySelectorAll(".number");
 const equalButton = document.querySelector(".operation.equal");
+const delButton = document.querySelector(".del");
+
+/*
+To do: 
+Make DEL button work 
+I can't do multiple values yet, like 50 - 5 + 10 = 55, so i need to figure that out. 
+Check if value1 and valu2 actually has values, before we can calculate, else make an error message. 
+0 / 0 = make other error than NaN.
+Get rid of the empty button.
+Maybe make design more like this: https://mrbuddh4.github.io/calculator/ 
+*/
 
 /* global scope variables */
 let currentState = "value1";
@@ -12,8 +23,16 @@ let value2 = "";
 let operationsToString = "";
 let operation = "";
 
-/* clear the calc */
+/* DEL button  */
+let test = "Hello there";
+console.log(test);
+delButton.addEventListener("click", () => {
+  let s = test.slice(0, -1);
+  console.log(s);
+  return s;
+});
 
+/* clear the calc */
 const clearAll = () => {
   reset.addEventListener("click", (e) => {
     numberPrint.innerText = "";
@@ -23,7 +42,7 @@ const clearAll = () => {
     currentState = "value1";
   });
 };
-clearAll();
+
 /* pressing the equal button */
 equalButton.addEventListener("click", () => {
   let result = operate(operationsToString, value1, value2);
